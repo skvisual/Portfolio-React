c5376f2b99dc8081a2863d981570734d168d30c4
skvisual <southkvisual@gmail.com>
2020-05-02 19:22:56 -0500

    finished cheers main info page

--- src/components/pages/CheersPage.js
+++ src/components/pages/CheersPage.js
@@ -4,7 +4,7 @@ import Navigation from "../navbar";
 import Row from 'react-bootstrap/Row';
 import Footer from '../footer';
 import PageNavLinks from '../PageNavLinks';
-import CheersScroller from '../CheersScroller';
+// import CheersScroller from '../CheersScroller';
 import Col from 'react-bootstrap/Col';
 
 import '../../App.css';
@@ -22,20 +22,17 @@ function CheersPage() {
                 <Row>
                     <div className="jumbotron jumbotron-fluid jtron box">
                         <div className="container">
-                            <h1 className="display-2 infoheader">cheers!</h1>
-                            <p className="infosubheader">see who's smartest. see who's funniest.</p>
-                            <p className="infosubheader">cheers.</p>
-
-                        </div>
-                            <Col sm="6">
-                            <p className="infosubheader subheadertext">cheers! is an online, table-game inspired multiplayer application. It utilizes several technologies; And delivers a fun, fast-paced experience to enjoy with your favorite people. </p>
-                            <br></br>
-                                
+                            <Col>
+                                <div className="jtronText">
+                                    <h1 className="display-4">cheers!</h1>
+                                    <p>see who's smartest. see who's funniest.</p>
+                                    <p>cheers.</p>
+                                    <p>cheers! is an online, table-game inspired  multiplayer application.</p>
+                                    <p>It utilizes several technologies to deliver a fun, fast-paced experience to enjoy with your favorite people. </p>
+                                </div>
                             </Col>
-                            
-
+                        </div>
                     </div>
-
                 </Row>
                 <br></br>
                 <br></br>
