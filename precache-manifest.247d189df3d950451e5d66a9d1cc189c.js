3106b2465a049e86f6d28e526831be1afb8398a2
skvisual <southkvisual@gmail.com>
2020-05-03 06:05:44 -0500

    Updates

---
+++ precache-manifest.247d189df3d950451e5d66a9d1cc189c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea36f1cbadfdd2a8aac62e2a354cc659",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "8c25a012b6a97da9cfeb",
+    "url": "/portfolio-react/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "2502a92980b4deac91c9",
+    "url": "/portfolio-react/static/css/main.342e9dd0.chunk.css"
+  },
+  {
+    "revision": "8c25a012b6a97da9cfeb",
+    "url": "/portfolio-react/static/js/2.a8a790a5.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/portfolio-react/static/js/2.a8a790a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2502a92980b4deac91c9",
+    "url": "/portfolio-react/static/js/main.954523e5.chunk.js"
+  },
+  {
+    "revision": "f3b68bd1632d2a5c2c2c",
+    "url": "/portfolio-react/static/js/runtime-main.45b98d22.js"
+  }
+]);
